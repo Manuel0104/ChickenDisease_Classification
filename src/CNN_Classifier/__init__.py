849d9c30117e62d62a24fbc5758fc4111889a393
Manuel0104 <manuelfer912@gmail.com>
2024-01-24 00:58:57 +0900

    implementation

---
+++ src/CNN_Classifier/__init__.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import logging
+
+#defining the logging format
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+
+#setting up the log dir and file path 
+log_dir = "logs"
+log_filepath = os.path.join(log_dir,"running_logs.log")
+os.makedirs(log_dir, exist_ok=True)
+
+
+#configuring the logging system 
+
+logging.basicConfig(
+    level= logging.INFO,
+    format= logging_str,
+
+    handlers=[
+        logging.FileHandler(log_filepath),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+#creating a logger instance
+logger = logging.getLogger("cnnClassifierLogger")
